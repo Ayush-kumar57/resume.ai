3c3877400765dc05092ebba3dc15bfea4a2c1169
Ayush-kumar57 <ayushkumarlp57707@gmail.com>
2026-02-08 11:25:02 +0530

    add button in ResumeBuilder.jsx

--- src/pages/ResumeBuilder.jsx
+++ src/pages/ResumeBuilder.jsx
@@ -130,6 +130,10 @@ const ResumeBuilder = () => {
                     onChange={(data)=>setResumeData(prev=>({...prev,skills:data}))} />
                   )}
                 </div>
+                <button className='bg-linear-to-br from-green-100 to-green-200
+                ring-green-300 text-green-600 ring hover:ring-green-400 transition-all rounded-md px-6 py-2 mt-6 text-sm'>
+                  Save Changes
+                </button>
               </div>
 
             </div>
