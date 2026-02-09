617b7de4cb9e486426e1150689cbe64f7412dd16
Ayush-kumar57 <ayushkumarlp57707@gmail.com>
2026-02-09 14:27:11 +0530

    .

--- client/src/components/ProjectForm.jsx
+++ client/src/components/ProjectForm.jsx
@@ -1,4 +1,4 @@
-import { Dice1 } from 'lucide-react';
+import { Plus, Trash2 } from 'lucide-react';
 import React from 'react'
 
 const ProjectForm = ({data, onChange}) => {
@@ -6,9 +6,11 @@ const ProjectForm = ({data, onChange}) => {
             const newProject = {
                name:"",
                type:"",
+               description:"",
+
 
             };
-            onChange([...data,newEducation])
+            onChange([...data,newProject])
 
     }
     const removeProject =(index)=>{
@@ -24,7 +26,57 @@ const ProjectForm = ({data, onChange}) => {
     }
 
   return (
-<div></div>
+ <div>
+        
+            <div className='flex items-center justify-between'>
+            <div>
+                <h3 className='flex items-center gap-2 text-lg font-semibold text-gray-900'>Project</h3>
+                <p className='text-sm text-gray-500'>Add your Project</p>
+            </div>
+            <button onClick={addProject} className='flex items-center gap-2 px-3 py-1 text-sm bg-green-100
+            text-green-700 rounded-lg hover:bg-green-200 transition-colors'>
+                <Plus className='size-4'/>
+                Add Project
+            </button>
+
+        
+        </div> 
+        
+            
+      
+            <div className='space-y-4 mt-6'>
+                {data.map((project,index)=>(
+                    <div key={index} className='p-4 border border-gray-200 rounded-lg
+                    space-y-3'>
+                        <div className='flex justify-between items-center'>
+                            <h4>Project #{index + 1}</h4>
+                            <button onClick={()=>removeProject(index)} className='text-red-500 hover:text-red-700 transition-colors'>
+                                <Trash2 className='size-4'/>
+                            </button>
+
+                        </div>
+                        <div className='grid gap-3'>
+
+                            <input value={project.name || ""} onChange={(e)=>updateProject(index,"name",e.target.value)}
+                             type="text" placeholder='Project Name' 
+                            className='px-3 py-2 text-sm rounded-lg'/>
+
+                              <input value={project.type || ""} onChange={(e)=>updateProject(index,"type",e.target.value)}
+                             type="text" placeholder='Project Type' 
+                            className='px-3 py-2 text-sm rounded-lg'/>
+
+                              <textarea rows={4} value={project.description || ""} onChange={(e)=>updateProject(index,"description",e.target.value)}
+                              placeholder='Describe your project...' 
+                            className='w-full px-3 py-2 text-sm rounded-lg resize-none'/>
+
+                        </div>
+                    </div>
+                ))}
+
+            </div>
+      
+        
+    </div>
     
   )
 }
--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import { Link,useParams } from 'react-router-dom'
+import { data, Link,useParams } from 'react-router-dom'
 import { dummyResumeData } from '../assets/assets'
-import { ArrowLeftIcon, Briefcase, ChevronLeft, ChevronRight, FileText, FolderIcon, GraduationCap, Sparkle, User } from 'lucide-react'
+import { ArrowLeftIcon, Briefcase, ChevronLeft, ChevronRight, DownloadIcon, EyeIcon, EyeOffIcon, FileText, FolderIcon, GraduationCap, Share2Icon, Sparkle, User } from 'lucide-react'
 import PersonalInfoForm from '../components/PersonalInfoForm'
 import ResumePreview from '../components/ResumePreview'
 import TemplateSelector from '../components/TemplateSelector'
@@ -9,6 +9,8 @@ import ColorPicker from '../components/ColorPicker'
 import ProfessionalSummaryform from '../components/ProfessionalSummaryform'
 import ExperianceForm from '../components/ExperianceForm'
 import EducationForm from '../components/EducationForm'
+import ProjectForm from '../components/ProjectForm'
+import Skillsform from '../components/Skillsform'
 
 
 const ResumeBuilder = () => {
@@ -50,6 +52,26 @@ const ResumeBuilder = () => {
   useEffect(()=>{
     loadExistingResume()
   },[])
+
+  const changeResumeVisibility =async () => {
+    setResumeData({...resumeData,public:!resumeData.public})
+  }
+
+  const handleShare  =()=>{
+    const frontendUrl =  window.location.href.split('/app/')[0];
+    const resumeUrl = frontendUrl + '/view/' +resumeId;
+
+    if(navigator.share){
+      navigator.share({url:resumeUrl,text:"My Resume"})
+      
+    }else{
+      alert("Share not supported on thuis browser.")
+    }
+  }
+
+  const dowloadResume  =()=>{
+    window.print();
+  }
   return (
     <div>
         
@@ -117,18 +139,43 @@ const ResumeBuilder = () => {
                     <EducationForm data={resumeData.education}
                     onChange={(data)=>setResumeData(prev=>({...prev,education:data}))} />
                   )}
+
+                  {activeSection.id === 'projects' && (
+                    <ProjectForm data={resumeData.project}
+                    onChange={(data)=>setResumeData(prev=>({...prev,project:data}))} />
+                  )}
                   
-                  
-                  
+                  {activeSection.id === 'skills' && (
+                    <Skillsform data={resumeData.skills}
+                    onChange={(data)=>setResumeData(prev=>({...prev,skills:data}))} />
+                  )}
                 </div>
+                <button className='bg-linear-to-br from-green-100 to-green-200
+                ring-green-300 text-green-600 ring hover:ring-green-400 transition-all rounded-md px-6 py-2 mt-6 text-sm'>
+                  Save Changes
+                </button>
               </div>
 
             </div>
 
             {/* {Right Panel - Preview} */}
             <div className='lg:col-span-7 max-lg:mt-6'>
-                <div>
-                  {/* --- buttons --- */}
+                <div className='relative w-full'>
+                  <div className='absolute bottom-3 left-0 right-0 flex items-center justify-end gap-2'>
+                    {resumeData.public && (
+                      <button onClick={handleShare} className='flex items-center p-2 px-4 gap-2 text-sx bg-linear-to-br from-blue-100 to-blue-200 text-blue-600 rounded-lg ring-blue-300 hover:ring transition-colors '>
+                        <Share2Icon className='Size-4'/>Share
+                      </button>
+                    )}
+                    <button onClick={changeResumeVisibility} className='flex items-center p-2 px-4 gap-2 text-sx bg-linear-to-br from-purple-100 to-purple-200 text-purple-600 ring-purple-300  hover:ring transition-colors'>
+                      {resumeData.public ? <EyeIcon className='size-4'/>:<EyeOffIcon className='size-4'/>}
+                      {resumeData.public ? "Public":"Private"}
+                    </button>
+                    <button onClick={dowloadResume} className='flex items-center px-6 py-2 gap-2 text-sx bg-linear-to-br from-green-100 to-green-200 text-green-600 rounded-lg ring-green-300 hover:ring transition-colors'>
+                    <DownloadIcon className='size-4'/>Dowlnoad
+                    </button>
+
+                  </div>
 
                 </div>
                 <ResumePreview data={resumeData} template={resumeData.template} 
